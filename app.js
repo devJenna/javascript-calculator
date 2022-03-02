94338e0c6e5d493ea5bb3078cac00d1b2420b42e
devJenna <devjennalewis@gmail.com>
2022-03-01 16:43:01 -0800

    update class name

--- app.js
+++ app.js
@@ -2,55 +2,141 @@ const currentOutput = document.querySelector(".current-output");
 const clearButton = document.querySelector(".clear");
 const plminusButton = document.querySelector(".pl-minus");
 const percentButton = document.querySelector(".percent");
-const decimalButton = document.querySelector(".decimal");
 const resultButton = document.querySelector(".operator-result");
-
+let prevVal = "";
+let newVal = "";
+let operator = "";
+let resultVal = "";
+let decimalClicked = false;
 
 clearButton.addEventListener("click", function () {
+    prevVal = "";
+    newVal = "";
+    operator = "";
+    resultVal = "";
     currentOutput.value = "";
 });
 
 plminusButton.addEventListener("click", function () {
-    currentOutput.value = (currentOutput.value) * -1;
+    resultVal = currentOutput.value * -1;
+    currentOutput.value = resultVal;
 });
 
 percentButton.addEventListener("click", function () {
-    currentOutput.value = currentOutput.value * 0.01;
+    resultVal = currentOutput.value * 0.01;
+    currentOutput.value = resultVal;
 });
-
-decimalButton.addEventListener("click", function () {
-    currentOutput.value += ".";
-});
-
 
 // using for loop to avoid adding id and getting value from each element
 const numberButton = document.querySelectorAll(".number");
 for (let i = 0; i < numberButton.length; i++) {
     numberButton[i].addEventListener("click", function () {
-        currentOutput.value += numberButton[i].value;
+        // currentOutput.value += numberButton[i].value;
         // console.log(numberButton[i]);
         // console.log(currentOutput.value);
-    })
-}
+        if (resultVal) {
+            newVal = numberButton[i].value;
+            // newVal += numberButton[i].value;
+            resultVal = "";
+            // console.log(newVal);
+            // console.log(resultVal);
+        } else {
+            // if the decimal button has been clicked
+            if (numberButton[i].value === ".") {
+                if (decimalClicked != true)
+                    newVal += numberButton[i].value;
+                decimalClicked = true;
+            }
+            //if the number button has been clicked
+            else {
+                newVal += numberButton[i].value;
+                // newVal = numberButton[i].value;
+            }
+        }
+        currentOutput.value = newVal;
+        // currentOutput.value += newVal;
+        // console.log(prevVal); 
+        // console.log(newVal); 
+    });
+};
 
 const operatorButton = document.querySelectorAll(".operator");
 for (let i = 0; i < operatorButton.length; i++) {
     operatorButton[i].addEventListener("click", function () {
+        if (!resultVal) {
+            prevVal = newVal;
+            // console.log(newVal); 
+            // console.log(prevVal); 
+        } else {
+            prevVal = resultVal;
+            // console.log(newVal); 
+            // console.log(prevVal); 
+            // console.log(resultVal);
+        }
+        newVal = "";
+        decimalClicked = false;
+        operator = operatorButton[i].value;
+        // console.log(operator);
+        resultVal = "";
+
+        // remove operator sign from currentOutput
+        // currentOutput.value = ""; 
+
+        // let operator sign visible
         currentOutput.value += operatorButton[i].value;
     })
-}
+};
 
-
-//using eval
 resultButton.addEventListener("click", function () {
-    currentOutput.value = eval(currentOutput.value);
+    decimalClicked = false;
+    prevVal = parseFloat(prevVal);
+    newVal = parseFloat(newVal);
+
+    // console.log(prevVal);
+    // console.log(operator);
+    // console.log(newVal);
+    // console.log(currentOutput.value); 
+
+    switch (operator) {
+        case "+":
+            resultVal = prevVal + newVal;
+            break;
+        case "-":
+            resultVal = prevVal - newVal;
+            break;
+        case "*":
+            resultVal = prevVal * newVal;
+            break;
+        case "/":
+            resultVal = prevVal / newVal;
+            break;
+
+        default: //keep the value
+            resultVal = newVal;
+    }
+
+    // console.log(prevVal);
+    // console.log(operator);
+    // console.log(newVal);
+    // console.log(resultVal);
+
+    prevVal = resultVal;
+    currentOutput.value = resultVal;
+
+    // console.log(prevVal);
+    // console.log(operator);
+    // console.log(newVal);
+    // console.log(resultVal);
+    // console.log(currentOutput.value);
 });
 
 
 
 
-
-
+//using eval
+// resultButton.addEventListener("click", function () {
+//     currentOutput.value = eval(currentOutput.value);
+// });
 
 // for loop replaces this part, not necessary to use id
 // const n1 = document.getElementById("n1");
@@ -121,5 +207,3 @@ resultButton.addEventListener("click", function () {
 // addButton.addEventListener("click", function () {
 //     currentOutput.value += "+"
 // });
-
-
